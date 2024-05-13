034d3f21db644f56b321642594a1eedad3ba339f
tyVespA <migliavacca.marco08@gmail.com>
2024-05-13 08:52:40 +0200

    Create server component, add it to App

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
+// component decorator - a decorator enhances ts classes (and other elements) - decorators are defined with the @ symbol
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
+
+// a component is a typescript class
 export class ServerComponent {}
