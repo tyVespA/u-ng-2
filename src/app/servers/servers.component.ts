17059fddec1990b88af7f0c7684a11371b3f9057
tyVespA <migliavacca.marco08@gmail.com>
2024-05-14 09:17:58 +0200

    Test property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css'
+  styleUrl: './servers.component.css',
 })
 export class ServersComponent {
+  allowNewServer = false;
 
+  // method executed when the component is created by angular
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 }
